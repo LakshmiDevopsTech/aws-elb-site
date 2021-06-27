303b7dca56d40ca4fd3ca16676ffbb6662887644
LakshmiDevopsTech <85107381+LakshmiDevopsTech@users.noreply.github.com>
2021-06-27 13:51:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ https://www.tooplate.com/view/2114-pixie
                <?php
 $output = shell_exec('echo $HOSTNAME');
 echo "<h1><center><pre>$output</pre></center></h1>";
-echo "<h1><center>This is my Website mounted with ALB </center></h1>"
+echo "<h1><center>This is my Website 1 </center></h1>"
 ?>
             </span>
           </div>
